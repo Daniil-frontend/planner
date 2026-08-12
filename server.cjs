e1b9c1d35fc32cf60a81dc530326905b83bad618
Vin66613 <daniilarsentev4056@gmail.com>
2026-08-12 19:26:19 +0300

    Switch to Railway

--- server.cjs
+++ server.cjs
@@ -6,6 +6,17 @@ const app = express();
 app.use(cors({ origin: '*' }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
 // Подключение к базе данных
 const pool = new Pool({
     user: 'postgres',
